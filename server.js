9a1186847ce0f39c554c743a0ebf6efd76ece973
Truong Nhat Tien <167666290+tientrader@users.noreply.github.com>
2025-02-27 21:42:24 +0700

    v1.2

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 const app = express();
 app.get('/', (req, res) => {
   res.send(
-    `<body style='background-color:#283E5B'><h1 style='color: orange;text-align:center'>Hello, I'm Truong Nhat Tien v1.1</h1></body>`
+    `<body style='background-color:#283E5B'><h1 style='color: orange;text-align:center'>Hello, I'm Truong Nhat Tien v1.2</h1></body>`
   );
 });
 
